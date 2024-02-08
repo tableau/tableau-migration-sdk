4a2910f30c3a096ac6abc410e2bdd2ce117ea491
Steffen Froehlich <sfroehlich@tableau.com>
2024-02-08 13:17:39 -0800

    true version of tags/release/1.0.0. Certain files were deleted but got moved anyway

--- tests/Tableau.Migration.TestComponents/Hooks/Mappings/TestTableauCloudUsernameOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Tableau.Migration.TestComponents.Hooks.Mappings
-{
-    public sealed class TestTableauCloudUsernameOptions
-    {
-        public string BaseOverrideMailAddress { get; set; } = string.Empty;
-    }
-}
